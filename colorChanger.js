8720548d1c1b6d774daae7e9f385dff05f2670fa
Lenovo <jusnos>
2018-12-19 11:48:03 +0100

    color changer

---
+++ colorChanger.js
@@ -0,0 +1,10 @@
+
+const items = document.querySelectorAll('div');
+
+items.forEach(function (item) {
+    item.addEventListener('click',changeColor)
+});
+
+function changeColor(){
+    document.body.className=this.className;
+}
